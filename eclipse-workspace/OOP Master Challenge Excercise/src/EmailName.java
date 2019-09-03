aefa54245d9aabd34ddf0771356a001028a55506
TirrellWestfield <40174070+TirrellWestfield@users.noreply.github.com>
2019-09-03 13:43:19 -0400

    email assignment

---
+++ eclipse-workspace/OOP Master Challenge Excercise/src/EmailName.java
@@ -0,0 +1,17 @@
+
+public class EmailName {
+	   
+
+	public static void main(String []args) {
+	         String name ="TW";
+	         String domain= "@nextrevtech.com"; 
+	         String name2 = "CG";
+	         String name3 = "JM"; 
+	         System.out.println(name + domain);
+	         System.out.println(name2 + domain);
+	         System.out.println(name3 + domain);
+	         
+	 
+	}
+	
+	 }
